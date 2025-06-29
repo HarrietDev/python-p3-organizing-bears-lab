4b5c68b13a10286d4418cfcadd8e996d93bacd02
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-06-29 21:53:40 +0300

    Perform CRUD operations on relational databases using SQL

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO bears (name,age,sex,color,temperament,alive 
+) VALUES 
+('Mr. Chocolate', 8, 'M', 'Brown', 'Playful', 1),
+('Rowdy', 12, 'M', 'Black', 'Serious', 1),
+('Tabitha', 5, 'F', 'Golden', 'Curious', 1),
+('Sergeant Brown', 6, 'F', 'Yellow', 'Friendly', 1),
+('Melissa', 15, 'F', 'Gray', 'Relaxed', 1),
+('Grinch', 10, 'M', 'Brown', 'Aggressive', 0),
+('Wendy', 4, 'F', 'White', 'Gentle', 1),
+(NULL, 3, 'M', 'Brown', 'Energetic', 1);
